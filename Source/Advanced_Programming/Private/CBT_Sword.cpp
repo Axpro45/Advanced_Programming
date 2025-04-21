a573f5d5d316583c39cc52300191f67a4df1b732
Axpro45 <167152988+Axpro45@users.noreply.github.com>
2025-04-21 19:13:03 +1000

    remade the c++ class

---
+++ Source/Advanced_Programming/Private/CBT_Sword.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CBT_Sword.h"
+
+// Sets default values
+ACBT_Sword::ACBT_Sword()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACBT_Sword::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACBT_Sword::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
